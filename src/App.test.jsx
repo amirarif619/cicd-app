f1be29b945febb6d93466b5da6d5b01c72de4255
Amir Arif <amirarif619@gmail.com>
2024-10-10 11:33:16 +0800

    first commit

---
+++ src/App.test.jsx
@@ -0,0 +1,21 @@
+import { render, screen } from "@testing-library/react";
+import App from "./App"
+import { expect, test } from "vitest";
+import userEvent from "@testing-library/user-event";
+
+
+test("App form submission", async () => {
+    render(<App />);
+
+        // Get the email input and submit button
+        const emailInput = screen.getByTestId("emailInput");
+        const submitButton = screen.getByTestId("submitButton");
+
+        // Type a valid email into the input
+        await userEvent.type(emailInput, 'sigmaschool@gmail.com');
+
+        //Check if button is disabled
+
+        expect(submitButton.disabled).toBe(false);
+
+})
